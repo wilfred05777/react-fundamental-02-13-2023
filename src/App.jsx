cf3a3c8ee806e5b3d3a3fe9fcc52687334e87c0d
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-02-15 04:36:01 +0800

    26. JSX - CSS

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,18 @@ const Image = () => (
 )
 const Title = () => <h2>Interesting Facts of Curious Minds</h2>
 const Author = () => {
-  return <h4>Jordan Moore</h4>
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem'
+  }
+
+  return (
+    <>
+      {/* <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}> */}
+      <h4 style={inlineHeadingStyles}>Jordan Moore</h4>
+    </>
+  )
 }
 
 export default App
